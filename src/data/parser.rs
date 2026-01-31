72e0aa8e910fc794c5068df46d7f6e068ab4cbd7
Inseok Lee <git@dlun.ch>
2026-01-31 15:26:51 +0900

    Fix clippy collapsible_if warning

--- src/data/parser.rs
+++ src/data/parser.rs
@@ -145,10 +145,10 @@ pub fn parse_maps(data: &str) -> Vec<Map> {
                     builder.dungeons.push((x, y, target));
                 }
             }
-        } else if !line.is_empty() && current_map.is_some() {
-            if let Some(ref mut builder) = current_map {
-                builder.add_row(line);
-            }
+        } else if !line.is_empty()
+            && let Some(ref mut builder) = current_map
+        {
+            builder.add_row(line);
         }
     }
 
