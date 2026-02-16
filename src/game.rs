8e24812f0f985536054e361ab963093d791f4d8b
Inseok Lee <git@dlun.ch>
2026-02-16 20:30:29 +0900

    Rename runtime_event module to game_event

--- src/game.rs
+++ src/game.rs
@@ -1,6 +1,6 @@
 mod game_data;
+mod game_event;
 mod rendering;
-mod runtime_event;
 mod save;
 mod save_schema;
 mod selection;
@@ -10,15 +10,15 @@ mod systems;
 mod ui;
 
 pub use game_data::GameData;
+pub use game_event::{
+    AppExploreEvent, AppMovementEvent, CombatRuntimeEvent, GameEvent, SessionEvent, ShopInputEvent,
+    TransitionEvent, UiEvent,
+};
 pub use rendering::{
     COLOR_CYAN, COLOR_DARK_GRAY, COLOR_GREEN, COLOR_RED, COLOR_WHITE, ExploreRender,
     InventoryRender, QuestLogRender, RenderState, ShopRender, StatsRender, build_render_state,
     clear_screen, draw_dialog, draw_explore, draw_inventory, draw_menu, draw_pause_menu,
     draw_quest_log, draw_rect, draw_shop, draw_stats, draw_text, fill_rect, render,
-};
-pub use runtime_event::{
-    AppExploreEvent, AppMovementEvent, CombatRuntimeEvent, GameEvent, SessionEvent, ShopInputEvent,
-    TransitionEvent, UiEvent,
 };
 pub use save::{has_save_data, load_game, save_game};
 pub use session::SessionState;
