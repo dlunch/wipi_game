f995127e617fd373fbabf73983b8acf7b45181b1
Inseok Lee <git@dlun.ch>
2026-02-17 09:21:58 +0900

    Preallocate event queue capacity

--- src/engine.rs
+++ src/engine.rs
@@ -160,7 +160,10 @@ impl GameEngine {
             return;
         }
 
-        let mut queue: VecDeque<GameEvent> = initial_events.into();
+        let mut queue = VecDeque::with_capacity(64);
+        for event in initial_events {
+            queue.push_back(event);
+        }
         let mut processed = 0usize;
 
         while let Some(event) = queue.pop_front() {
