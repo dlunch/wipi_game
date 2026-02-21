026d780b8b4141d5e1335633d9ffed377f297401
Inseok Lee <git@dlun.ch>
2026-02-21 13:00:26 +0900

    Use release build

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-cargo -Zbuild-std=core,alloc build --target thumbv4t-none-eabi --features ktf --profile dev --no-default-features
-cargo run --manifest-path ../wipi/Cargo.toml -p wipi_archiver -- ktf target/thumbv4t-none-eabi/debug/wipi_game Clet 00000000 PD000000 ./resources > target/wipi_game_ktf.zip
+cargo -Zbuild-std=core,alloc build --target thumbv4t-none-eabi --features ktf --profile release --no-default-features
+cargo run --manifest-path ../wipi/Cargo.toml -p wipi_archiver -- ktf target/thumbv4t-none-eabi/release/wipi_game Clet 00000000 PD000000 ./resources > target/wipi_game_ktf.zip
 
-cargo -Zbuild-std=core,alloc build --target thumbv4t-none-eabi --features lgt --profile dev --no-default-features
-cargo run --manifest-path ../wipi/Cargo.toml -p wipi_archiver -- lgt target/thumbv4t-none-eabi/debug/wipi_game Clet 00000000 PD000000 ./resources > target/wipi_game_lgt.zip
+cargo -Zbuild-std=core,alloc build --target thumbv4t-none-eabi --features lgt --profile release --no-default-features
+cargo run --manifest-path ../wipi/Cargo.toml -p wipi_archiver -- lgt target/thumbv4t-none-eabi/release/wipi_game Clet 00000000 PD000000 ./resources > target/wipi_game_lgt.zip
